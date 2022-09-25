bd5465aa5c0d428410052b23ef2b928dcf399a21
unknown <sams.kool10@gmail.com>
2022-09-25 13:58:56 +0545

    some changes

--- tats.sql
+++ tats.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2022 at 01:54 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Sep 25, 2022 at 10:13 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `tats`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `number` varchar(12) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `method` varchar(50) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `package_id` int(11) NOT NULL,
+  `placed_on` varchar(50) NOT NULL,
+  `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`id`, `name`, `number`, `email`, `method`, `address`, `package_id`, `placed_on`, `payment_status`) VALUES
+(11, 'asadf', '234234', 'sdf@sdf.com', 'cash on delivery', 'Booking , , asdf, sdf - sdf', 6, '25-Sep-2022', 'pending');
 
 -- --------------------------------------------------------
 
@@ -44,32 +69,18 @@ CREATE TABLE `cart` (
 
 CREATE TABLE `message` (
   `id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
   `message` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `Booking`
+-- Dumping data for table `message`
 --
 
-CREATE TABLE `Booking` (
-  `id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `number` varchar(12) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `method` varchar(50) NOT NULL,
-  `address` varchar(500) NOT NULL,
-  `total_packages` varchar(1000) NOT NULL,
-  `total_price` int(100) NOT NULL,
-  `placed_on` varchar(50) NOT NULL,
-  `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `message` (`id`, `name`, `email`, `number`, `message`) VALUES
+(11, 'hello', 'sas@sdf.com', '234234', 'sfd');
 
 -- --------------------------------------------------------
 
@@ -83,6 +94,18 @@ CREATE TABLE `packages` (
   `price` int(100) NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`id`, `name`, `price`, `image`) VALUES
+(6, 'bouddha', 1000, 'pic-2.png'),
+(7, 'dhucdsu', 899000, 'i (2).jpg'),
+(8, 'hucdhcjh', 90990, 'i (2).jpg'),
+(9, 'hdhdjhj', 90909, 'i (1).jpg'),
+(10, 'dhjdhcjd', 909909, 'pic-1.png'),
+(11, 'hgddgcd', 8980, 'i (3).jpg');
 
 -- --------------------------------------------------------
 
@@ -99,8 +122,24 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
+(1, 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 'user'),
+(2, 'sams', 'admin@admin1.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'user'),
+(3, 'sams admin', 'sams@admin2.com', '1a1dc91c907325c69271ddf0c944bc72', 'admin'),
+(4, 'admin1', 'admin1@gmail.com', '712123cd000f534df53cfdf739865da9', 'admin');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `cart`
@@ -112,12 +151,6 @@ ALTER TABLE `cart`
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `booking`
---
-ALTER TABLE `Booking`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -137,33 +170,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `cart`
+--
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `Booking`
---
-ALTER TABLE `Booking`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `packages`
 --
 ALTER TABLE `packages`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
